f910835bea097d8253334abc5f6c4072d638680f
Brian Stanley <bmstanley87@gmail.com>
2018-01-14 14:44:34 -0500

    Update food generation to try to prevent food position colliding with the snake, change keydown events to look at the 'key' property of the event, and implement first run at having the game play itself

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -1,11 +1,21 @@
 const canvas = document.getElementById("snake");
 const context = canvas.getContext("2d");
 
+const keyCodeDown = { keyCode: 40, key: 'ArrowDown' };
+const keyCodeUp = { keyCode: 38, key: 'ArrowUp' };
+const keyCodeRight = { keyCode: 39, key: 'ArrowRight' };
+const keyCodeLeft = { keyCode: 37, key: 'ArrowLeft' };
+
 const box_size = 32;
 const playable_area_height = 15;
 const playable_area_width = 17;
 const left_offset = 1;
 const top_offset = 3;
+
+const left_boundary = box_size;
+const right_boundary = playable_area_width * box_size;
+const top_boundary = top_offset * box_size;
+const bottom_boundary = (top_offset + playable_area_height - 1) * box_size;
 
 const ground = new Image();
 ground.src="assets/img/ground.png";
@@ -45,16 +55,16 @@ function setDirection(event) {
     if (game_running === false) {
         return false;
     }
-    if (event.keyCode == 37 && direction != "RIGHT") {
+    if (event.key == keyCodeLeft.key && direction != "RIGHT") {
         left_audio.play();
         direction = "LEFT";
-    } else if (event.keyCode == 38 && direction != "DOWN") {
+    } else if (event.key == keyCodeUp.key && direction != "DOWN") {
         up_audio.play();
         direction = "UP";
-    } else if (event.keyCode == 39 && direction != "LEFT") {
+    } else if (event.key == keyCodeRight.key && direction != "LEFT") {
         right_audio.play();
         direction = "RIGHT";
-    } else if (event.keyCode == 40 && direction != "UP") {
+    } else if (event.key == keyCodeDown.key && direction != "UP") {
         down_audio.play();
         direction = "DOWN";
     }
@@ -108,7 +118,7 @@ function draw() {
     }
 
     // game over
-    if (snakeX < box_size || snakeX > playable_area_width * box_size || snakeY < top_offset * box_size || snakeY > playable_area_width * box_size || collision(newHead, snake)) {
+    if (snakeX < left_boundary || snakeX > right_boundary || snakeY < top_boundary || snakeY > bottom_boundary || collision(newHead, snake)) {
         clearInterval(game);
         dead_audio.play();
         game_running = false;
@@ -120,17 +130,93 @@ function draw() {
     context.font = "45px Open Sans";
     // offset the score text by 2 boxes on left and 1.7 boxes on top
     context.fillText(score, box_size * 2, box_size * 1.7);
+
+    autoplay();
 }
 
 function createFood() {
-    let potential = {
-        x : Math.floor(Math.random() * playable_area_width + left_offset) * box_size,
-        y : Math.floor(Math.random() * playable_area_height + top_offset) * box_size,
-    }
-    if (collision(potential, snake)) {
-        createFood();
-    }
+    do {
+        var potential = {
+            x : Math.floor(Math.random() * playable_area_width + left_offset) * box_size,
+            y : Math.floor(Math.random() * playable_area_height + top_offset) * box_size,
+        }
+    } while (collision(potential, snake));
     return potential;
+}
+
+function autoplay() {
+    if (snake[0].x == food.x) {
+        if (snake[0].y < food.y) {
+            if (direction == "UP") {
+                triggerKeydown(keyCodeLeft);
+            } else {
+                triggerKeydown(keyCodeDown);
+            }
+        } else if (snake[0].y > food.y) {
+            if (direction == "DOWN") {
+                triggerKeydown(keyCodeLeft);
+            } else {
+                triggerKeydown(keyCodeUp);
+            }
+        }
+    } else if (snake[0].y == food.y) {
+        if (snake[0].x < food.x) {
+            if (direction == "LEFT") {
+                triggerKeydown(keyCodeUp);
+            } else {
+                triggerKeydown(keyCodeRight);
+            }
+        } else if (snake[0].x > food.x) {
+            if (direction == "RIGHT") {
+                triggerKeydown(keyCodeUp);
+            } else {
+                triggerKeydown(keyCodeLeft);
+            }
+        }
+    } else if (snake[0].x == left_boundary && snake[0].y == top_boundary) {
+        if (direction == "UP") {
+            triggerKeydown(keyCodeRight);
+        } else {
+            triggerKeydown(keyCodeDown);
+        }
+    } else if (snake[0].x == left_boundary && snake[0].y == bottom_boundary) {
+        if (direction == "DOWN") {
+            triggerKeydown(keyCodeRight);
+        } else {
+            triggerKeydown(keyCodeUp);
+        }
+    } else if (snake[0].x == right_boundary && snake[0].y == top_boundary) {
+        if (direction == "UP") {
+            triggerKeydown(keyCodeLeft);
+        } else {
+            triggerKeydown(keyCodeDown);
+        }
+    } else if (snake[0].x == right_boundary && snake[0].y == bottom_boundary) {
+        if (direction == "DOWN") {
+            triggerKeydown(keyCodeLeft);
+        } else {
+            triggerKeydown(keyCodeUp);
+        }
+    } else if (snake[0].x == left_boundary) {
+        triggerKeydown(keyCodeDown);
+    } else if (snake[0].x == right_boundary) {
+        triggerKeydown(keyCodeDown);
+    } else if (snake[0].y == top_boundary) {
+        triggerKeydown(keyCodeLeft);
+    } else if (snake[0].y == bottom_boundary) {
+        triggerKeydown(keyCodeRight);
+    }
+}
+
+function triggerKeydown(code) {
+    let event = new KeyboardEvent('keydown', {
+        bubbles: true,
+        cancelable: true,
+        code: code.key,
+        composed: true,
+        key: code.key
+    });
+    document.dispatchEvent(event);
 }
 
 // call the draw function every 100 ms
